82d3e45517d106e71564131814cf2421c5a97f13
OajacaAllison <oajacaallison@gmail.com>
2025-06-11 13:58:27 -0400

    what

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,4 @@
-let disk1 = document.querySelector ("work");
-disk1.addEventListener('click', function(){
-    Animation.apply
+let work = document.querySelector ("workButton");
+workButton.addEventListener('click', function(){
+    disk1.style.animation= "animation: 3s linear 1s infinite running slide-in;"
 })
